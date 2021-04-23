464e041cf5ccda6bbe065a99b0c337dda6fdbe40
akarki102444 <82452752+qwertyytrewqaka@users.noreply.github.com>
2021-04-23 10:32:00 +0900

    Update clock.py

--- clock.py
+++ clock.py
@@ -8,7 +8,7 @@ def timed_job():
     main.main()
 
 
-sched.add_job(timed_job, 'interval', seconds=10, max_instances=3, jitter=1)
+sched.add_job(timed_job, 'interval', seconds=10, jitter=1)
 
 
 sched.start()
