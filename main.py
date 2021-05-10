3cd02a69aa4d2a6997df5124d3391c8ce3751b12
akarki102444 <82452752+qwertyytrewqaka@users.noreply.github.com>
2021-05-10 22:18:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import constants
-import sys
 
 def nt_notify():
     with requests.get("https://newtoki95.com/toki_bl",  headers=constants.headers) as req:
@@ -34,16 +33,16 @@ def nt_notify():
                                                        "번째 새 글이 올라왔어요!" + '\n' + "다운로드 수" + ": " + downloads)
                         print(number, "번째", "새 공유탭 글이 올라옴")
 
-                    elif int(downloads) > 0 and link not in constants.history:
-                        constants.history.insert(0, link)
-                        constants.history.pop()
-                        print(constants.history)
-                        constants.bot.sendMessage(constants.chat_id,
-                                                  text="첫번째 글:" + "\n" + "분류: " + category + " " + time + ": " + "\n" + member + "님의 "
-                                                       + str(number) + "번째 새 글이 올라왔어요!" + '\n' + "다운로드 수" + ": " + downloads
-                                                  + "\n" + "제목: " + title)
+            if int(downloads) > 0 and link not in constants.history and category != "공유":
+                constants.history.insert(0, link)
+                constants.history.pop()
+                print(constants.history)
+                constants.bot.sendMessage(constants.chat_id,
+                                          text="첫번째 글:" + "\n" + "분류: " + category + " " + time + ": " + "\n" + member + "님의 "
+                                               + str(number) + "번째 새 글이 올라왔어요!" + '\n' + "다운로드 수" + ": " + downloads
+                                               + "\n" + "제목: " + title)
 
-                        print(number, "번째", "새 공유탭에 없는 공유글이 올라옴(1번째글)", "분류: ", category)
+                print(number, "번째", "새 공유탭에 없는 공유글이 올라옴(1번째글)", "분류: ", category)
 
             number = soup.select("#list-body > li > div.wr-num.hidden-xs")[7].text
             time = soup.select("#list-body > li > div.wr-date.hidden-xs > span")[4].text
@@ -53,7 +52,6 @@ def nt_notify():
             member = soup.select("#list-body > li > div.wr-name.hidden-xs > a > span")[7].text[1:].lstrip()
             category = soup.select("#list-body > li > div.wr-subject > a > span.tack-icon")[7].text
             link = soup.select("#list-body > li > div.wr-subject > a")[7]['href']
-
             print("두번째:", number, time, downloads, title, member, category, link)
 
             if int(downloads) > 0 and link not in constants.history and category != "공유" and not (time[-2] == '분' and int(time[:-2]) > 10):
