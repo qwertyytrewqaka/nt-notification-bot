691ada7b2d766b3052448c7c4857741dc32fea5c
akarki102444 <83045232+test-account184@users.noreply.github.com>
2021-06-08 00:22:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ from bs4 import BeautifulSoup
 import os
 import constants
 
+
 def nt_notify():
-    with requests.get("https://newtoki95.com/toki_bl",  headers=constants.headers) as req:
+    with requests.get("https://newtoki95.com/toki_bl", headers=constants.headers) as req:
         html = req.text
         soup = BeautifulSoup(html, 'html.parser')
-        number = soup.select("#list-body > li > div.wr-num.hidden-xs")[6].text
-        time = soup.select("#list-body > li > div.wr-date.hidden-xs > span")[3].text
-        downloads = soup.select("#list-body > li > div.wr-down.hidden-xs")[6].text[1:].rstrip()
-        title = soup.select("#list-body > li > div.wr-subject > a")[6].text.partition("  ")[2].rpartition(" ")[0]
-        member = soup.select("#list-body > li > div.wr-name.hidden-xs > a > span")[6].text[1:].lstrip()
-        category = soup.select("#list-body > li > div.wr-subject > a > span.tack-icon")[6].text
-        link = soup.select("#list-body > li > div.wr-subject > a")[6]['href']
+        posts = soup.select("#list-body > li")
+        for i in range(1, 10):
+            if posts[i].select("div.wr-num.hidden-xs")[0].text not in ["", "*"]:
+                constants.latest_number = i
+                break
+        number = posts[constants.latest_number].select("div.wr-num.hidden-xs")[0].text
+        time = posts[constants.latest_number].select("div.wr-date.hidden-xs")[0].text[1:].rstrip()
+        downloads = posts[constants.latest_number].select("div.wr-down.hidden-xs")[0].text[1:].rstrip()
+        title = posts[constants.latest_number].select("div.wr-subject > a")[0].text.partition("  ")[2].rpartition(" ")[0]
+        member = posts[constants.latest_number].select("div.wr-name.hidden-xs > a > span")[0].text[1:].lstrip()
+        category = posts[constants.latest_number].select("div.wr-subject > a > span.tack-icon")[0].text
+        link = posts[constants.latest_number].select("div.wr-subject > a")[0]['href']
 
         print("첫번째:", number, time, downloads, title, member, category, link)
 
@@ -29,7 +35,8 @@ def nt_notify():
 
                     if category == "공유" and not (time[-2] == '분' and int(time[:-2]) > 1):
                         constants.bot.sendMessage(constants.chat_id,
-                                                  text="분류: 공유" + "\n" + time + ": " + "\n" + member + "님의 " + str(number) +
+                                                  text="분류: 공유" + "\n" + time + ": " + "\n" + member + "님의 " + str(
+                                                      number) +
                                                        "번째 새 글이 올라왔어요!" + '\n' + "다운로드 수" + ": " + downloads)
                         print(number, "번째", "새 공유탭 글이 올라옴")
 
@@ -44,17 +51,18 @@ def nt_notify():
 
                 print(number, "번째", "새 공유탭에 없는 공유글이 올라옴(1번째글)", "분류: ", category)
 
-            number = soup.select("#list-body > li > div.wr-num.hidden-xs")[7].text
-            time = soup.select("#list-body > li > div.wr-date.hidden-xs > span")[4].text
-            downloads = soup.select("#list-body > li > div.wr-down.hidden-xs")[7].text[1:].rstrip()
-            title = \
-                soup.select("#list-body > li > div.wr-subject > a")[7].text.partition("  ")[2].rpartition(" ")[0]
-            member = soup.select("#list-body > li > div.wr-name.hidden-xs > a > span")[7].text[1:].lstrip()
-            category = soup.select("#list-body > li > div.wr-subject > a > span.tack-icon")[7].text
-            link = soup.select("#list-body > li > div.wr-subject > a")[7]['href']
+            number = posts[constants.latest_number+1].select("div.wr-num.hidden-xs")[0].text
+            time = posts[constants.latest_number+1].select("div.wr-date.hidden-xs")[0].text[1:].rstrip()
+            downloads = posts[constants.latest_number+1].select("div.wr-down.hidden-xs")[0].text[1:].rstrip()
+            title = posts[constants.latest_number+1].select("div.wr-subject > a")[0].text.partition("  ")[2].rpartition(" ")[0]
+            member = posts[constants.latest_number+1].select("div.wr-name.hidden-xs > a > span")[0].text[1:].lstrip()
+            category = posts[constants.latest_number+1].select("div.wr-subject > a > span.tack-icon")[0].text
+            link = posts[constants.latest_number+1].select("div.wr-subject > a")[0]['href']
+
             print("두번째:", number, time, downloads, title, member, category, link)
 
-            if 50 > int(downloads) > 0 and link not in constants.history and category != "공유" and not (time[-2] == '분' and int(time[:-2]) > 10):
+            if 50 > int(downloads) > 0 and link not in constants.history and category != "공유" and not (
+                    time[-2] == '분' and int(time[:-2]) > 10):
                 constants.history.insert(0, link)
                 constants.history.pop()
                 print(constants.history)
@@ -64,7 +72,7 @@ def nt_notify():
                                                + "\n" + "제목: " + title)
 
                 print(number, "번째", "새 공유탭에 없는 공유글이 올라옴(2번째글)", "분류: ", category)
-                
+
 
 def main():
     try:
@@ -78,7 +86,7 @@ def main():
             constants.errorcount += 1
         if constants.errorcount == 50:
             constants.bot.sendMessage(constants.chat_id,
-                                          text="서버 오류/기타 문제 생김")
+                                      text="서버 오류/기타 문제 생김")
         constants.server_state = 1
         print("에러가 발생했습니다. (IndexError) 다시 연결하는 중...", constants.errorcount)
 
@@ -87,5 +95,6 @@ def main():
             constants.errorcount = 0
             if constants.errorcount >= 50:
                 constants.bot.sendMessage(constants.chat_id, text="서버 오류/기타 문제 해결됨")
-                    
+
         constants.server_state = 0
+
