2533a6863a8af1fd5c55a49e0bac2c769a9624b3
qwertyytrewqaka <82452752+qwertyytrewqaka@users.noreply.github.com>
2021-07-31 02:39:12 +0900

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
 import telegram
 import os
 
-token = '1940162290:AAFo04xoU4sYzHT2EF-Kp_R_gyu2Kr_e9Sc'
-chat_id = "1693156867"
+token = '1924062828:AAFLaI7oB564URemtLmOFRYk1icGf4jTCCk'
+chat_id = "-1001520088936"
 bot = telegram.Bot(token=token)
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
